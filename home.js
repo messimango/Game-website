7d0af179818d82f2d57175147b6541d191723a50
Umang Poudyal <umangpoudyal@gmail.com>
2022-03-24 12:57:50 -0400

    added momo clicker

--- home.js
+++ home.js
@@ -1,18 +1,45 @@
-const games = [
+const games = [    
     {
-        id: 1,
-        name: "Snake",
-        image: "./images/snake.png",
-        link: "./snake/snake.html",
-        description: "Controls a long, thin creature, resembling a snake, which roams around on a bordered plane, picking up apples, trying to avoid hitting its own body or the edges of the playing area.",
+        id: 9,
+        name: "Momo Clicker",
+        image: "./images/momoClicker.png",
+        link: "./MomoClicker/momo.html",
+        description: "An idle clicker game. Start you momo empire and take over the world.",
     },
-    
+    {
+        id: 8,
+        name: "Tetris",
+        image: "./images/tetris.png",
+        link: "./Tetris/tetris.html",
+        description: "The classic game of tetris, the game has a simple goal of destroying lines of block before it reaches the top.",
+    },     
+    {
+        id: 5,
+        name: "One Piece Card Match",
+        image: "./images/OPcards.png",
+        link: "./CardMatch/card.html",
+        description: "A card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.",
+    },    
     {
         id: 2,
         name: "Tic Tac Toe",
         image: "./images/tictac.PNG",
         link: "./TicTacToe/tictac.html",
         description: "Two players seek in alternate turns to complete a row, a column, or a diagonal with either three O's or three X's drawn in the spaces of a grid of nine squares.",
+    },    
+    {
+        id: 7,
+        name: "Pac Man",
+        image: "",
+        link: "./Pac-Man/pacman.html",
+        description: "The classic game of Pac-Man. Eat coins and run or fight the ghosts",
+    },    
+    {
+        id: 1,
+        name: "Snake",
+        image: "./images/snake.png",
+        link: "./snake/snake.html",
+        description: "Controls a long, thin creature, resembling a snake, which roams around on a bordered plane, picking up apples, trying to avoid hitting its own body or the edges of the playing area.",
     },
     {
         id: 3,
@@ -29,14 +56,6 @@ const games = [
         link: "./Drop/drop.html",
         description: "A ball in freefall must avoid to roof and fall through the cracks in the floor.",
     },
-    {
-        id: 5,
-        name: "One Piece Card Match",
-        image: "./images/OPcards.png",
-        link: "./CardMatch/card.html",
-        description: "A card game in which all of the cards are laid face down on a surface and two cards are flipped face up over each turn. The object of the game is to turn over pairs of matching cards.",
-    }, 
-
     {
         id: 6,
         name: "Jumper",
