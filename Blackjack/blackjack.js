090a92d0e31ff65f844e27e69c0ed168184c46f1
Umang Poudyal <umangpoudyal@gmail.com>
2022-03-30 23:01:35 -0400

    Added Blackjack

---
+++ Blackjack/blackjack.js
@@ -0,0 +1,78 @@
+const deck = [
+    {
+        value : 2,
+        cardnumber : '2',
+        cardsuit : 'd',
+    },
+]
+
+const playerAmount = document.querySelector('.player-amount')
+const dealerAmount = document.querySelector('.dealer-amount')
+
+var dealer = 0;
+var player = 0;
+
+function hit() {
+    if (dealer == 0) {
+        player = Math.floor(Math.random()*10 + 1);
+        dealer = Math.floor(Math.random()*10 + 1); 
+        playerAmount.innerHTML = player;
+        dealerAmount.innerHTML = dealer;
+    } else if (dealer != 0 && player < 21) {
+        console.log('hitagain')
+        player = player + Math.floor(Math.random()*10 + 1);
+        playerAmount.innerHTML = player;
+    }
+    checkPlayer();
+}
+
+const endtext = document.querySelector('.endtext');
+
+function checkPlayer() {
+    if (player == 21) {
+        stay();
+    } else if (player > 21) {
+        endtext.innerHTML = 'You Lose!'
+        gameend.classList.remove('hidden')
+    }
+}
+
+const gameend = document.querySelector('.gameend')
+
+function playAgain() {
+    player = 0;
+    dealer = 0;
+    playerAmount.innerHTML = player;
+    dealerAmount.innerHTML = dealer;
+    gameend.classList.add('hidden');
+}
+
+
+
+function stay() {
+    if (dealer == 0) {
+        console.log('return')
+        return
+    } else if (dealer < 21 && dealer <= player) {
+        console.log('stay')
+        dealer = dealer + Math.floor(Math.random()*10 + 1);
+        dealerAmount.innerHTML = dealer;
+        checkDealer();       
+    }
+}
+
+
+function checkDealer() {
+    if (dealer > 21) {
+        endtext.innerHTML = 'You Win!'
+        gameend.classList.remove('hidden')
+    } else if (dealer > player) {
+        endtext.innerHTML = 'You Lose!'
+        gameend.classList.remove('hidden')
+    } else if (dealer < player) {
+        stay();
+    } else if (dealer == 21 && player == 21) {
+        endtext.innerHTML = "It's a Tie!"
+        gameend.classList.remove('hidden')
+    }
+}
